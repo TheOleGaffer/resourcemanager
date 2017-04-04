3e11c757ec7b73ce525dab6580d101d8a3381d92
Sam P. Rensenhouse <Sam.Rensenhouse@goilluminate.com>
2017-04-03 23:53:14 -0500

    Added more tables

--- ResourceManager.Database/Manager/Tables/Session.sql
+++ ResourceManager.Database/Manager/Tables/Session.sql
@@ -2,5 +2,8 @@
 (
 	[SessionId] INT Identity(1,1) PRIMARY KEY, 
     [DungeonMasterId] INT NOT NULL, 
-    [SessionName] NVARCHAR(50) NOT NULL
+    [SessionName] NVARCHAR(50) NULL, 
+    [CreatedOnDate] DATETIME NOT NULL DEFAULT (getutcdate()), 
+    [NonActive] INT NOT NULL DEFAULT 0, 
+    CONSTRAINT [FK_Session_Account] FOREIGN KEY ([DungeonMasterId]) REFERENCES [DungeonMaster]([DungeonMasterId])
 )
