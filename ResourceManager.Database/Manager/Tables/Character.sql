2c9c398c0434c7c5e032934f6ea186b83a4989cd
Sam P. Rensenhouse <Sam.Rensenhouse@goilluminate.com>
2017-04-24 19:19:05 -0500

    Fixing merge messup

--- ResourceManager.Database/Manager/Tables/Character.sql
+++ ResourceManager.Database/Manager/Tables/Character.sql
@@ -1,10 +1,7 @@
 ﻿CREATE TABLE [dbo].[Character]
 (
-<<<<<<< HEAD
 	[CharacterId] INT NOT NULL PRIMARY KEY, 
-=======
 	[CharacterId] INT Identity(1,1) PRIMARY KEY, 
->>>>>>> a39effcc455f7df00f366d57b1351fbd64ffad9b
     [UserId] INT NOT NULL, 
     [CampaignId] INT NOT NULL, 
     [CharacterName] NVARCHAR(50) NOT NULL DEFAULT 'Character', 
